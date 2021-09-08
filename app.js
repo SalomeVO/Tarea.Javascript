c0e63930531da4201ed7b111be59b409d44c8297
salomeV <mvasquezo10@miumg.edu.gt>
2021-09-07 22:55:44 -0600

    comandos para interactuar con la consola mensajes de saludos y errores

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-/* Para escribir en el documento
-document.write("Hola mundo");*/
+/* Para escribir en el documento 
+document.write("<h1>Hola mundo</h1>"); */
+
+/*Para interactuar con la consola*/
+console.log("Hola Mundo"); 
+console.error("La apliacion a fallado");
