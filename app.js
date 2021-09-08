a5540cccb57b4902bce225609941da2afe4ed6b9
salomeV <mvasquezo10@miumg.edu.gt>
2021-09-07 23:15:24 -0600

    tipos de datos: string

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 /* Para escribir en el documento 
 document.write("<h1>Hola mundo</h1>"); */
 
-/*Para interactuar con la consola*/
+/* Para interactuar con la consola
 console.log("Hola Mundo"); 
-console.error("La apliacion a fallado");
+console.error("La apliacion a fallado"); */
+
+/*Tipos de datos
+string = 'cadena de texto' o "cadena de texto" */
+
+console.log("esto es una cadena de texto");
+
+/*number = 1000 o -2.3
+boolean = true o false 
+array = ['joe', 'ryan', 'martha'] o [1,2,3] o [true, false, true]
+
+object
+ { 
+  "nombre": "rayn", 
+  "puntuacion": 70.4, 
+  "horas": 14, 
+  "es profesional": true
+  "amigos": ['', "", ""]
+  }
+
+  { 
+  "nombre": "joe", 
+  "puntuacion": 0.4, 
+  "horas": 1, 
+  "es profesional": false
+  }
+*/
