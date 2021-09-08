0b2343dfdb6229167fc45e9860f652451524efa5
salomeV <mvasquezo10@miumg.edu.gt>
2021-09-07 23:49:18 -0600

    variables y constantes

--- app.js
+++ app.js
@@ -19,7 +19,20 @@ console.log(true);*/
 /*array = ['joe', 'ryan', 'martha'] o [1,2,3] o [true, false, true]
 console.log([1, 2, 3, 4]);*/
 
-/*object*/
-
+/*object
 console.log({"nombre": "rayn", "puntuacion": 70.4, "horas": 14, "es profesional": true, "amigos": ['', "", ""] });
 console.log({"nombre": "joe", "puntuacion": 0.4, "horas": 1, "es profesional": false });
+*/
+
+/*Variables: como su nombre lo dice son datos que no son fijos y pueden cambiar constantemente
+*/
+
+var nameUser = "john";
+let apellido = "carter";
+
+nameUser = 'pepe' 
+
+const PI = 3.1415;
+//PI = 100; error porque una const no cambia su valor
+
+console.log(nameUser + " " + apellido);
